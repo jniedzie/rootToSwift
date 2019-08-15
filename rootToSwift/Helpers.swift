5629dcd57bc509241bdb16e9f47eca889364082e
jniedzie <jeremi.niedziela@cern.ch>
2019-08-15 15:44:25 +0200

    Cleaning up the project.

---
+++ rootToSwift/Helpers.swift
@@ -0,0 +1,76 @@
+//
+//  Helpers.swift
+//  rootToSwift
+//
+//  Created by Jeremi Niedziela on 15/08/2019.
+//  Copyright © 2019 Jeremi Niedziela. All rights reserved.
+//
+
+import Foundation
+
+enum RSError: Error {
+  /// noArgumentList Provided Method object's arguments field is nil
+  case noArgumentList
+  /// noReturnOrName Provided method string didn't contain name or return type
+  case noReturnOrName
+}
+
+/// Dictionary mapping custom root types to standard C types
+let rootTypes = [
+  "Char_t" : "char",
+  "UChar_t" : "unsigned char",
+  "Short_t" : "short",
+  "UShort_t" : "unsigned short",
+  "Int_t" : "int",
+  "UInt_t" : "unsigned int",
+  "Seek_t" : "int",
+  "Long_t" : "long",
+  "ULong_t" : "unsigned long",
+  "Float_t" : "float",
+  "Float16_t" : "float",
+  "Double_t" : "double",
+  "Double32_t" : "double",
+  "LongDouble_t" : "long double",
+  "Text_t" : "char",
+  "Bool_t" : "bool",
+  "Byte_t" : "unsigned char",
+  "Version_t" : "short",
+  "Option_t" : "const char",
+  "Ssiz_t" : "int",
+  "Real_t" : "float",
+  "Long64_t" : "long long",
+  "ULong64_t" : "unsigned long long",
+  "Axis_t" : "double",
+  "Stat_t" : "double",
+  "Font_t" : "short",
+  "Style_t" : "short",
+  "Marker_t" : "short",
+  "Width_t" : "short",
+  "Color_t" : "short",
+  "SCoord_t" : "short",
+  "Coord_t" : "double",
+  "Angle_t" : "float",
+  "Size_t" : "float",
+  
+  "TObject" : "SObject",
+]
+
+/**
+ Returns current date in dd/m/yyyy format as a string
+ */
+func getCurrentDate() -> String {
+  return "\(Calendar.current.component(.day, from: Date()))/\(Calendar.current.component(.month, from: Date()))/\(Calendar.current.component(.year, from: Date()))"
+}
+
+/**
+ Removes what looks like default arguments declaration in C from the provided string
+ */
+func stripDefaultValue(name:inout String) {
+  if let nameRange = name.range(of: #"(\w)*"#, options: .regularExpression) {
+    name = String(name[nameRange])
+  }
+}
+
+
+
+
--- rootToSwift/main.swift
+++ rootToSwift/main.swift
@@ -10,97 +10,40 @@ import Foundation
 
 let rootIncludePath = "/Applications/root_v6.16.00/include"
 let outputPath      = "/Users/jeremi/Library/Mobile Documents/com~apple~CloudDocs/Applications/swiftRoot/swiftRoot"
-let className       = "Canvas"
+let className       = "H2"
 
 let fileProcessor = FileProcessor()
 
-var text = fileProcessor.getContentsOfFile(path: "\(rootIncludePath)/T\(className).h")
-
-let publicMethods = fileProcessor.getPublicMethods(text: text)
-
-let currentDate:String = "\(Calendar.current.component(.day, from: Date()))/\(Calendar.current.component(.month, from: Date()))/\(Calendar.current.component(.year, from: Date()))"
-
+// Create Objective-C++ header and imlpementation files for requested ROOT class
+let currentDate = getCurrentDate()
 var headerText = fileProcessor.getHeaderBeginning(className: className, currentDate: currentDate)
 var implementationText = fileProcessor.getImplementationBeginning(className: className, currentDate: currentDate)
 
-print("Public methods:")
-for method in publicMethods {
-  do {
-    let methodPieces = try fileProcessor.getMethodPieces(method: method)
+// Get public methods from ROOT header
+var text = fileProcessor.getContentsOfFile(path: "\(rootIncludePath)/T\(className).h")
+let publicMethods = fileProcessor.getPublicMethods(text: text)
 
-    headerText += "-(\(methodPieces.returnType)) \(methodPieces.name)"
-    implementationText += "-(\(methodPieces.returnType)) \(methodPieces.name)"
-    
-    if methodPieces.arguments.count > 0 {
-      headerText += ":"
-      implementationText += ":"
-      
-      var first = true
-      for arg in methodPieces.arguments {
-        var argName = arg.name
-        if let nameRange = arg.name.range(of: #"(\w)*"#, options: .regularExpression) {
-          argName = String(arg.name[nameRange])
-        }
-        
-        if first {
-          first = false
-          headerText += "(\(arg.type)) \(argName) "
-          implementationText += "(\(arg.type)) \(arg.name) "
-        }
-        else {
-          headerText += "\(argName):(\(arg.type)) \(argName) "
-          implementationText += "\(argName):(\(arg.type)) \(arg.name) "
-        }
-      }
-    }
-    headerText += ";\n\n"
-    
-    if methodPieces.returnType == "SObject*" {
-      implementationText += """
-      {
-        TObject *obj = [self object]->\(methodPieces.name)(
-      """
-    }
-    else {
-      implementationText += """
-      {
-        return [self object]->\(methodPieces.name)(
-      """
-    }
-      
-    if methodPieces.arguments.count > 0 {
-      var first = true
-      
-      for arg in methodPieces.arguments {
-        if first { first = false }
-        else { implementationText += ", " }
-        implementationText += "\(arg.name)"
-      }
-    }
-    
-    if methodPieces.returnType == "SObject*" {
-      implementationText += """
-        );
-        CPPMembers *members = new CPPMembers(obj);
-        return [[SObject alloc] initWithObject:members];
-        }\n\n
-      """
-    }
-    else {
-      implementationText += """
-      );
-      }\n\n
-      """
-    }
+// Fill in header and implementation files with public ROOT class methods
+for method in publicMethods {
+  guard let methodPieces = Method(method: method)
+    else{
+      print("Could not translate string:\n\(method)\n to a method object")
+      continue;
   }
-  catch {
-    print("Could not get method pieces for method: \(method)")
-    continue
-  }
+  let methodDeclaration = "-(\(methodPieces.returnType)) \(methodPieces.name)"
+  
+  headerText += methodDeclaration
+  implementationText += methodDeclaration
+  
+  methodPieces.addArgumentsList(headerText: &headerText, implementationText: &implementationText)
+  methodPieces.addMethodImplementation(implementationText: &implementationText)
+  headerText += ";\n\n"
 }
 
+// Add endings of header and implementation files
 headerText += fileProcessor.getHeaderEnding(className: className)
 implementationText += fileProcessor.getImplementationEnding()
 
+// Save files
 fileProcessor.writeText(text: headerText, filePath: "\(outputPath)/S\(className).h")
 fileProcessor.writeText(text: implementationText, filePath: "\(outputPath)/S\(className).mm")
